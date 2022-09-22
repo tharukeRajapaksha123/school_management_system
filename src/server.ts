cda261e48b98f1498def78c7cb314906b6777121
tharukerajapaksha123 <darksparrow156@gmail.com>
2022-09-22 12:07:44 +0530

    project started

---
+++ src/server.ts
@@ -0,0 +1,63 @@
+import express from "express"
+import http from "http"
+import mongoose from "mongoose"
+import Logging from "./libraries/logging"
+
+
+const db_url = ""
+const port = 8080
+const router = express();
+
+
+mongoose.connect(db_url)
+    .then(() => { })    
+    .catch(err => Logging.error(err))
+
+const StartServer = ()=>{
+    router.use((req, res, next) => {
+        /** Log the req */
+        Logging.info(`Incomming - METHOD: [${req.method}] - URL: [${req.url}] - IP: [${req.socket.remoteAddress}]`);
+
+        res.on('finish', () => {
+            /** Log the res */
+            Logging.info(`Result - METHOD: [${req.method}] - URL: [${req.url}] - IP: [${req.socket.remoteAddress}] - STATUS: [${res.statusCode}]`);
+        });
+
+        next();
+    });
+
+    router.use(express.urlencoded({ extended: true }));
+    router.use(express.json());
+
+    /** Rules of our API */
+    router.use((req, res, next) => {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+
+        if (req.method == 'OPTIONS') {
+            res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+            return res.status(200).json({});
+        }
+
+        next();
+    });
+
+    /** Routes */
+    
+
+    /** Healthcheck */
+    router.get('/ping', (req, res, next) => res.status(200).json({ hello: 'world' }));
+
+    /** Error handling */
+    router.use((req, res, next) => {
+        const error = new Error('Not found');
+
+        Logging.error(error);
+
+        res.status(404).json({
+            message: error.message
+        });
+    });
+
+    http.createServer(router).listen(port, () => Logging.info(`Server is running on port ${port}`));
+}
